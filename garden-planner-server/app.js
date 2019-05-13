99288c4650339183c35982e9d761587e26bfb7b6
Shaun W Colley <swc7838@gmail.com>
2019-05-13 15:49:59 -0500

    calling api to create database of data

--- garden-planner-server/app.js
+++ garden-planner-server/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const axios = require('axios')
 const app = express()
 const PORT = process.env.PORT || 8080
 
@@ -11,7 +12,26 @@ app.get('/', (req,res) => {
   res.send('json will go here')
 })
 
+const getData = async () => {
+  try {
+    for (let i = 1; i < 22; i++){
+      let url = "https://www.growstuff.org/crops.json?page=" + i
+      const response = await axios.get(url)
+      const data = response.data
+      console.log(i)
+      console.log(data[0].name)
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+getData()
 
+/*
+arr.forEach(function callback(currentValue [, index [, array]]) {
+    //your iterator
+}[, thisArg]);
+*/
 app.listen(PORT,function(){
   console.log("Server is growing...")
 })
