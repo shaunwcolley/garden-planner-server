c816ff9dedb20566eb806028b9e880ca7698e740
Shaun Colley <swc7838@gmail.com>
2019-07-24 11:11:46 -0500

    added catch to middleware to auth so server doesn't crash

--- garden-planner-server/utils/authenticate.js
+++ garden-planner-server/utils/authenticate.js
@@ -3,22 +3,28 @@ const bodyParser = require('body-parser');
 
 function authenticate(req, res, next) {
   const headers = req.headers['authorization'];
-  const token = headers.split(' ')[1];
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if(decoded) {
-      if(decoded.userId) {
-        userId = parseInt(decoded.userId);
-        console.log(userId);
-        console.log('decoded');
-        next();
+  try {
+    const token = headers.split(' ')[1];
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if(decoded) {
+        if(decoded.userId) {
+          userId = parseInt(decoded.userId);
+          console.log(userId);
+          console.log('decoded');
+          next();
+        } else {
+          res.status(401).json({message: 'Invalid Token'});
+        };
       } else {
-        res.status(401).json({message: 'Invalid Token'});
+        res.status(401).json({message: 'Invalid Token', err: err});
       };
-    } else {
-      res.status(401).json({message: 'Invalid Token', err: err});
-    };
-  });
+    });
+  }
+  catch {
+    res.status(401).json({message: 'Error in trying to authenticate credentials, please try again.'})
+  }
+
+
 };
 
 module.exports = authenticate;
